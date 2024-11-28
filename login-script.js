2f5095f890b76c7b6a17eef336c038ce379c2809
RChakradharii <renukachakradhari23@navgurukul.org>
2024-11-28 10:25:57 +0530

    Initial commit for Library App

--- login-script.js
+++ login-script.js
@@ -1,11 +1,11 @@
 document.getElementById('login-student').addEventListener('click', () => {
-    window.location.href = 'library.html?role=student';
+    window.location.href = 'index.html?role=student';
 });
 
 document.getElementById('login-librarian').addEventListener('click', () => {
-    window.location.href = 'library.html?role=librarian';
+    window.location.href = 'index.html?role=librarian';
 });
 
 document.getElementById('login-admin').addEventListener('click', () => {
-    window.location.href = 'library.html?role=admin';
+    window.location.href = 'index.html?role=admin';
 });
